bb96d24ba0d98a61ca06df93ae46505f731c293e
Marko Lavrinec <marko.lavrinec@specto.si>
2020-11-28 12:17:37 +0100

    read me updated, build included in git

---
+++ build/precache-manifest.47c599b410f84db106d1ed6693227932.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7a4840beb123746ae67bc888cce3f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ac5832c85a8da283258",
+    "url": "/static/css/2.e820a691.chunk.css"
+  },
+  {
+    "revision": "430d7653859946bbe364",
+    "url": "/static/css/main.e058db38.chunk.css"
+  },
+  {
+    "revision": "4ac5832c85a8da283258",
+    "url": "/static/js/2.31f1888a.chunk.js"
+  },
+  {
+    "revision": "e88704e03699b0bc75037bb71207df3f",
+    "url": "/static/js/2.31f1888a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "430d7653859946bbe364",
+    "url": "/static/js/main.2732627f.chunk.js"
+  },
+  {
+    "revision": "29c4995013be4954e500",
+    "url": "/static/js/runtime-main.dcc7b5e5.js"
+  },
+  {
+    "revision": "cb37186282b5546030246d3e12c02448",
+    "url": "/static/media/aplikacija_drevesna_struktura.cb371862.jpg"
+  }
+]);
